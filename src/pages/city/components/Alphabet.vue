3985bf1fd64aad702a2e1698ccb438264442e405
wt <307548594@qq.com>
2018-06-28 15:59:20 +0800

    axios动态数据渲染

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,40 @@
+<template>
+    <ul class="list">
+        <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        cities: Object
+    },
+    data () {
+        return {
+
+        }
+    },
+    components: {
+
+    }
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl'
+    .list
+        display flex
+        flex-direction column
+        justify-content center
+        position absolute 
+        top 1.58rem
+        right 0
+        bottom 0
+        width .4rem
+        .item
+            line-height .4rem
+            text-align center
+            color $bgColor
+
+ 
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">当前城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="area">
+                <div class="title border-topbottom">热门城市城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper" v-for="item of hot" :key="item.id">
+                        <div class="button">{{item.name}}</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="area" v-for="(item,key) of cities" :key="key">
+                <div class="title border-topbottom">{{key}}</div>
+                <div class="item-list" v-for="item1 of item" :key="item1.id">
+                    <div class="item border-bottom">{{item1.name}}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll';
+export default {
+    // name: 'CityList',
+    props: {
+        hot: Array,
+        cities: Object
+    },
+    data () {
+        return {
+
+        }
+    },
+    mounted () { // DOM挂载完毕后执行
+        this.scroll = new Bscroll(this.$refs.wrapper,{
+            click: true
+        })
+    }
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl'
+    .border-topbottom
+        &:before
+            border-color #ccc
+        &:after
+            border-color #ccc
+    .border-bottom
+        &:before
+            border-color #ccc
+    .list
+        overflow hidden
+        position absolute
+        top 1.58rem
+        bottom 0
+        left 0
+        right 0
+        .title
+            line-height .54rem
+            background #eeeeee
+            padding-left .2rem
+            color #666666
+            font-size .26rem
+        .button-list
+            padding .1rem .6rem .1rem .1rem
+            overflow hidden
+            .button-wrapper
+                float left
+                width 33.33%
+                .button
+                    margin .1rem
+                    padding .1rem 0
+                    text-align center
+                    border .02rem solid #cccccc
+                    border-radius .06rem
+        .item-list
+            .item
+                line-height .76rem
+                padding-left .2rem
+
+</style>
