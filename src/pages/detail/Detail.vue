23a6ad9fdef55ff4e067a0eb03f4f15324841444
wt <307548594@qq.com>
2018-07-01 14:09:13 +0800

    详情组件header动画

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
         <detail-banner></detail-banner>
+        <detail-header></detail-header>
+        <div class="content"></div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
     data () {
         return {
@@ -13,12 +16,15 @@ export default {
         }
     },
     components: {
-        DetailBanner
+        DetailBanner,
+        DetailHeader
     }
 }
 </script>
 
 <style lang='stylus' scoped>
 
+    .content
+        height 50rem
  
 </style>
