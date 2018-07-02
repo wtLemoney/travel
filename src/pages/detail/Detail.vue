228e31424c4804c70460013ba7f9f666cd55cd26
wt <307548594@qq.com>
2018-07-02 21:43:25 +0800

    详情页列表项组件递归

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,22 +2,37 @@
     <div>
         <detail-banner></detail-banner>
         <detail-header></detail-header>
-        <div class="content"></div>
+        <div class="content">
+            <detail-list :list="list"></detail-list>
+        </div>
+        
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
     data () {
         return {
-
+            list: [{
+                title: '成人票',
+                children: [{
+                    title: '成人单人套餐',
+                    children: [{
+                        title: '成人单人半价'
+                    }]
+                }]
+            },{
+                title: '学生票'
+            }]
         }
     },
     components: {
         DetailBanner,
-        DetailHeader
+        DetailHeader,
+        DetailList
     }
 }
 </script>
